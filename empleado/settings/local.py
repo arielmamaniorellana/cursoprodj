f913efb8c758e704ad4d78e86a43e2efe1285232
Ariel Mamani <arielmamaniorellana12@gmail.com>
2021-09-25 13:33:49 -0300

    archivo prod

--- empleado/settings/local.py
+++ empleado/settings/local.py
@@ -23,6 +23,7 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static'] # carpeta base static
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
